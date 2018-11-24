37ca681b1bfe3fe32e127599b1fd7af9de8e67c9
André Souza Nunes <Andr Souza Nunes>
2018-11-23 22:45:39 -0400

    Atualização do Babel, Eslint, DevTool e reactTron

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Button } from 'react-native';
 
-import './config/ReactotronConfig';
-import './config/DevToolsConfig';
+import 'config/ReactotronConfig';
+import 'config/DevToolsConfig';
 
 import Todo from './components/Todo';
 
 const styles = StyleSheet.create({
+  // eslint-disable-next-line react-native/no-color-literals
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -17,23 +18,33 @@ const styles = StyleSheet.create({
 
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+  }
   state = {
     todos: [{ id: 0, text: 'Estudar JavaScript' },
       { id: 1, text: 'Estudar React Native' },
       { id: 2, text: 'Estudar Oracle' },
+      { id: 3, text: 'Estudar PL/SQL' },
+
     ],
   }
 
   addTodo = () => {
-    this.setState({
-      todos: [...this.state.todos, { id: Math.random(), text: 'Estudar React' }],
-    });
+    const { todos } = this.state;
+    this.setState(todos => [...todos, { id: Math.random(), text: 'Estudar React' }]);
+
+    // this.setState({
+    //   todos: [...this.state.todos, { id: Math.random(), text: 'Estudar React' }],
+    // });
   }
 
   render() {
+    const { todos } = this.state;
     return (
       <View style={styles.container}>
-        {this.state.todos.map(todo => <Todo key={todo.id} title={todo.text} />)}
+        {todos.map(todo => <Todo key={todo.id} title={todo.text} />)}
         <Button title="Adcionar todo" onPress={this.addTodo} />
       </View>
     );
